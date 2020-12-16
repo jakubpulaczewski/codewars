45346347749e309decbdaa44f951da16c7d8b1c9
jakubpulaczewski <yellowbrown4@gmail.com>
2020-12-16 16:41:19 +0000

    Update balanced-number.py

--- 7-kyu/balanced-number.py
+++ 7-kyu/balanced-number.py
@@ -5,6 +5,10 @@ The link: https://www.codewars.com/kata/5a4e3782880385ba68000018
 
 Problem Description:
 Balanced number is the number that * The sum of all digits to the left of the middle digit(s) and the sum of all digits to the right of the middle digit(s) are equal*.
+If the number has an odd number of digits then there is only one middle digit, e.g. 92645 has middle digit 6; otherwise, there are two middle digits , e.g. 1301 has middle digits 3 and 0
+The middle digit(s) should not be considered when determining whether a number is balanced or not, e.g 413023 is a balanced number because the left sum and right sum are both 5.
+
+
   
 Examples:
     (balanced-num 7) ==> return "Balanced"
