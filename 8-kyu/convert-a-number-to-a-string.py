25a097f095dce868c436fd545533de36e5ab7fc2
jakubpulaczewski <yellowbrown4@gmail.com>
2020-12-16 14:52:59 +0000

    Update convert-a-number-to-a-string.py

--- 8-kyu/convert-a-number-to-a-string.py
+++ 8-kyu/convert-a-number-to-a-string.py
@@ -1,1 +1,16 @@
 
+# Convert a Number to a String!
+# https://www.codewars.com/kata/5265326f5fda8eb1160004c8
+#
+# Problem Description: Implement the function that can trasnform a number into a string.
+#  
+# Examples:
+#   number_to_string(123) /* returns '123' */
+#   number_to_string(999) /* returns '999' */
+***
+def number_to_string(num):
+    return str(num)
+  
+  
+number_to_string(67)
+# The Expected result is '67'.
