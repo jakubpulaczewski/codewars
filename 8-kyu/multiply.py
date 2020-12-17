2516ac449dd1915b2bbb83aee95de482c95fa18e
jakubpulaczewski <yellowbrown4@gmail.com>
2020-12-17 13:21:40 +0000

    Create multiply.py

---
+++ 8-kyu/multiply.py
@@ -0,0 +1,15 @@
+"""
+Multiply
+
+The link: https://www.codewars.com/kata/50654ddff44f800200000004
+
+Problem Description:
+This code does not execute properly. Try to figure out why.
+
+Examples:
+    def multiply(a, b):
+      a * b
+"""
+
+def multiply(a, b):
+  return a * b
