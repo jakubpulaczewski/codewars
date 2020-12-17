c8fa3ad7816f1f689b2fdcfe71723f9598851c92
jakubpulaczewski <yellowbrown4@gmail.com>
2020-12-17 12:40:06 +0000

    Create check-the-examp.py

---
+++ 8-kyu/check-the-examp.py
@@ -0,0 +1,37 @@
+"""
+Check the exam.
+
+The link: https://www.codewars.com/kata/5a3dd29055519e23ec000074
+
+Problem Description:
+The first input array is the key to the correct answers to an exam, like ["a", "a", "b", "d"]. The second one contains a student's submitted answers.
+The two arrays are not empty and are the same length. Return the score for this array of answers, giving +4 for each correct answer, -1 for each incorrect
+answer, and +0 for each blank answer, represented as an empty string.
+
+If the score < 0, return 0.
+  
+Examples:
+      checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]) → 6
+      checkExam(["a", "a", "c", "b"], ["a", "a", "b",  ""]) → 7
+      checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]) → 16
+      checkExam(["b", "c", "b", "a"], ["",  "a", "a", "c"]) → 0
+"""
+
+
+def check_exam(arr1,arr2):
+    i = 0
+    sum = 0
+    while i < len(arr1):
+        
+        if arr1[i] == arr2[i]:
+            sum+=4
+        else:
+            if arr2[i] == "":
+                sum = sum + 0
+            else:
+                sum-=1
+        i+=1
+    if sum < 0:
+        return 0
+    return sum
+  
